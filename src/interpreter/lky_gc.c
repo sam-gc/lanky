2b45c6a3f373aaec16d3c2f9c4f206d23766bead
Sam Olsen <shorlbec@macalester.edu>
2015-04-04 14:38:09 +0100

    Fix bug causing crash during gc for blobs (don't know why valgrind didn't catch this...)

--- src/interpreter/lky_gc.c
+++ src/interpreter/lky_gc.c
@@ -201,7 +201,7 @@ void gc_mark_object(lky_object *o)
     
     o->mem_count = 1;
 
-    if(o->type != LBI_INTEGER && o->type != LBI_FLOAT &&
+    if(o->type != LBI_INTEGER && o->type != LBI_FLOAT && o->type != LBI_BLOB &&
             o->type != LBI_SEQUENCE && o->type != LBI_CODE && o->type != LBI_ITERABLE)
         hst_for_each(&o->members, gc_mark_for_each, NULL);
     //gc_mark_object(&o->parent);
