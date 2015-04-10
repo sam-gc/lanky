0ee61247032fda2fd731da678ede90fbf5630874
Sam Olsen <shorlbec@macalester.edu>
2015-04-10 16:24:14 +0100

    Switch Io/File libraries to new-style class format; add lots more error checking (it should be hard to segfault files now)

--- src/stdlib/stl_io.c
+++ src/stdlib/stl_io.c
@@ -27,15 +27,14 @@
 #include "class_builder.h"
 
 typedef struct {
+    unsigned read: 1;
+    unsigned write: 1;
+    unsigned open: 1;
     FILE *f;
-} stlio_file_object_data;
+} stlio_blob;
 
-lky_object *stlio_input(lky_func_bundle *bundle)
-{
-    lky_object_seq *args = BUW_ARGS(bundle);
-
-    lky_object *prompt = (lky_object *)args->value;
-    lobjb_print_object(prompt, BUW_INTERP(bundle));
+CLASS_MAKE_METHOD(stlio_prompt, self,
+    if($1) lobjb_print_object($1, interp_);
 
     char *buf = NULL;
     size_t sz = 0;
@@ -47,46 +46,217 @@ lky_object *stlio_input(lky_func_bundle *bundle)
     lky_object *toret = stlstr_cinit(buf);
     free(buf);
     return toret;
+)
+
+CLASS_MAKE_METHOD(stlio_put, self,
+    if($1) lobjb_print_object($1, interp_);
+)
+
+CLASS_MAKE_METHOD(stlio_putln, self,
+    if($1) lobjb_print($1, interp_);
+)
+
+CLASS_MAKE_METHOD_EX(stlio_file_readall, self, stlio_blob *, fb_,
+    CLASS_ERROR_ASSERT(fb_->read, "FileModeInvalid", "Attempted to read from write-only file.");
+    CLASS_ERROR_ASSERT(fb_->open, "FileStreamClosed", "The file stream has already been closed");
+
+    FILE *f = fb_->f;
+
+    long ct = 100;
+    char *running = calloc(ct, sizeof(char));
+    char *line = NULL;
+    size_t sz = 0;
+    while(getline(&line, &sz, f) != -1)
+    {
+        size_t ls = strlen(line);
+        if(strlen(running) + ls + 1 > ct)
+        {
+            ct *= 2;
+            char *back = running;
+            running = calloc(ct, sizeof(char));
+            strcpy(running, back);
+            free(back);
+        }
+
+        strcat(running, line); free(line); line = NULL;
+        sz = 0;
+    }
+
+    lky_object *ret = stlstr_cinit(running);
+    free(running);
+    if(line)
+        free(line);
+
+    lobj_set_member(self, "EOF", &lky_yes);
+
+    return ret;
+)
+
+CLASS_MAKE_METHOD_EX(stlio_file_readlines, self, stlio_blob *, fb_,
+    CLASS_ERROR_ASSERT(fb_->read, "FileModeInvalid", "Attempted to read from write-only file.");
+    CLASS_ERROR_ASSERT(fb_->open, "FileStreamClosed", "The file stream has already been closed");
+    FILE *f = fb_->f;
+
+    arraylist list = arr_create(10);
+
+    char *line = NULL;
+    size_t sz = 0;
+    while(getline(&line, &sz, f) != -1)
+    {
+        // Replace the newline character.
+        line[strlen(line) - 1] = '\0';
+        lky_object *str = stlstr_cinit(line);
+        free(line);
+        arr_append(&list, str);
+        line = NULL;
+        sz = 0;
+    }
+    
+    if(line)
+        free(line);
+
+    lobj_set_member(self, "EOF", &lky_yes);
+
+    return stlarr_cinit(list);
+)
+
+CLASS_MAKE_METHOD_EX(stlio_file_readline, self, stlio_blob *, fb_,
+    CLASS_ERROR_ASSERT(fb_->read, "FileModeInvalid", "Attempted to read from write-only file.");
+    CLASS_ERROR_ASSERT(fb_->open, "FileStreamClosed", "The file stream has already been closed");
+    FILE *f = fb_->f;
+
+    char *line = NULL;
+    size_t sz = 0;
+    if(getline(&line, &sz, f) < 0)
+    {
+        lobj_set_member(self, "EOF", &lky_yes);
+        if(line) free(line);
+        return &lky_nil;
+    }
+
+    line[strlen(line) - 1] = '\0';
+
+    lky_object *ret = stlstr_cinit(line);
+    free(line);
+    return ret;
+)
+
+CLASS_MAKE_METHOD_EX(stlio_file_write, self, stlio_blob *, fb_,
+    CLASS_ERROR_ASSERT(fb_->write, "FileModeInvalid", "Attempting to write to file in read-only mode");
+    CLASS_ERROR_ASSERT(fb_->open, "FileStreamClosed", "The file stream has already been closed");
+    if(!$1)
+        return &lky_nil;
+    FILE *f = fb_->f;
+
+    char *line = lobjb_stringify($1, interp_);
+
+    fprintf(f, "%s", line);
+
+    free(line);
+)
+
+CLASS_MAKE_METHOD_EX(stlio_file_writeline, self, stlio_blob *, fb_,
+    CLASS_ERROR_ASSERT(fb_->write, "FileModeInvalid", "Attempting to write to file in read-only mode");
+    CLASS_ERROR_ASSERT(fb_->open, "FileStreamClosed", "The file stream has already been closed");
+    FILE *f = fb_->f;
+
+    if(!$1)
+    {
+        fprintf(f, "\n");
+        return &lky_nil;
+    }
+
+    char *line = lobjb_stringify($1, interp_);
+    
+    fprintf(f, "%s\n", line);
+
+    free(line);
+)
+
+CLASS_MAKE_METHOD_EX(stlio_file_close, self, stlio_blob *, fb_,
+    fclose(fb_->f);
+    fb_->f = NULL;
+    fb_->open = 0;
+)
+
+static lky_object *stlio_file_class_ = NULL;
+lky_object *stlio_get_file_class()
+{
+    if(stlio_file_class_)
+        return stlio_file_class_;
+
+    CLASS_MAKE(cls, NULL, NULL, 0,
+        CLASS_STATIC_ONLY;
+        CLASS_PROTO("EOF", &lky_no);
+        CLASS_PROTO_METHOD("close", stlio_file_close, 0);
+        CLASS_PROTO_METHOD("getlns", stlio_file_readlines, 0);
+        CLASS_PROTO_METHOD("getln", stlio_file_readline, 0);
+        CLASS_PROTO_METHOD("getall", stlio_file_readall, 0);
+        CLASS_PROTO_METHOD("put", stlio_file_write, 1);
+        CLASS_PROTO_METHOD("putln", stlio_file_writeline, 1);
+    );
+
+    stlio_file_class_ = cls;
+    return cls;
 }
 
-lky_object *stlio_put(lky_func_bundle *bundle)
+CLASS_MAKE_BLOB_FUNCTION(stlio_file_blob_function, stlio_blob *, b, how,
+    if(how == CGC_FREE)
+        free(b);
+)
+
+void stlio_file_custom_init(lky_object *self, lky_object *cls, void *data)
 {
-    lky_object_seq *args = BUW_ARGS(bundle);
-
-    lky_object *obj = (lky_object *)args->value;
-    lobjb_print_object(obj, BUW_INTERP(bundle));
-
-    return &lky_nil;
+    CLASS_SET_BLOB(self, "fb_", data, stlio_file_blob_function);
 }
 
-lky_object *stlio_putln(lky_func_bundle *bundle)
+CLASS_MAKE_METHOD(stlio_fopen, cls, 
+    CLASS_ERROR_ASSERT($2, "MissingInformation", "Function requires filename and file mode.");
+    char *mode = stlstr_unwrap($2);
+
+    FILE *f = fopen(stlstr_unwrap($1), stlstr_unwrap($2));
+    CLASS_ERROR_ASSERT(f, "FileNotFound", "The file was not found or was not accessible for the given mode.");
+
+    stlio_blob *b = malloc(sizeof(stlio_blob));
+    b->write = 0;
+    b->read = 0;
+    b->open = 1;
+
+    b->f = f;
+    size_t len = strlen(mode);
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        if(mode[i] == '+')
+            b->read = b->write = 1; // TODO: I know this is wrong...
+        if(mode[i] == 'r' || mode[i] == 'R')
+            b->read = 1;
+        else if(mode[i] == 'w' || mode[i] == 'W' || mode[i] == 'a' || mode[i] == 'A')
+            b->write = 1;
+    }
+
+    return clb_instantiate(stlio_get_file_class(), stlio_file_custom_init, b);
+)
+
+static lky_object *stlio_class_ = NULL;
+lky_object *stlio_get_class()
 {
-    lky_object_seq *args = BUW_ARGS(bundle);
+    if(stlio_class_)
+        return stlio_class_;
 
-    lky_object *obj = (lky_object *)args->value;
-    lobjb_print(obj, BUW_INTERP(bundle));
-
-    return &lky_nil;
+    CLASS_MAKE(cls, NULL, NULL, 0,       
+        CLASS_STATIC_ONLY;
+        CLASS_STATIC_METHOD("prompt", stlio_prompt, 1);
+        CLASS_STATIC_METHOD("put", stlio_put, 1);
+        CLASS_STATIC_METHOD("putln", stlio_putln, 1);
+        CLASS_STATIC_METHOD("fopen", stlio_fopen, 2);
+    );
+    
+    stlio_class_ = cls;
+    return cls;
 }
 
 /*
-lky_object *stlio_printf(lky_func_bundle *bundle)
-{
-    lky_object_seq *args = BUW_ARGS(bundle);
-
-    arraylist list = arr_create(10);
-    MAKE_VA_ARGS(args, list, 1);
-    lobjb_print_object(stlstr_fmt_ext(((lky_object_custom *)args->value)->data, list), BUW_INTERP(bundle));
-
-    return &lky_nil;
-}
-*/
-
-void stlio_file_dealloc(lky_object_custom *obj)
-{
-    free(obj->data);
-}
-
 lky_object *stlio_file_read_raw(lky_func_bundle *bundle)
 {
     lky_object_function *func = BUW_FUNC(bundle);
@@ -114,206 +284,5 @@ lky_object *stlio_file_read_raw(lky_func_bundle *bundle)
     lobj_set_member((lky_object *)str, "length", lobjb_build_int(filelen));
 
     return (lky_object *)str;
-}
+}*/
 
-lky_object *stlio_file_readall(lky_func_bundle *bundle)
-{
-    lky_object_function *func = BUW_FUNC(bundle);
-
-    lky_object_custom *self = (lky_object_custom *)func->owner;
-    stlio_file_object_data *data = self->data;
-
-    FILE *f = data->f;
-
-    long ct = 100;
-    char *running = calloc(ct, sizeof(char));
-    char *line = NULL;
-    size_t sz = 0;
-    while(getline(&line, &sz, f) != -1)
-    {
-        size_t ls = strlen(line);
-        if(strlen(running) + ls + 1 > ct)
-        {
-            ct *= 2;
-            char *back = running;
-            running = calloc(ct, sizeof(char));
-            strcpy(running, back);
-            free(back);
-        }
-
-        strcat(running, line);
-        free(line);
-        line = NULL;
-        sz = 0;
-    }
-
-    lky_object *ret = stlstr_cinit(running);
-    free(running);
-    if(line)
-        free(line);
-
-    lobj_set_member((lky_object *)self, "EOF", lobjb_build_int(1));
-
-    return ret;
-}
-
-lky_object *stlio_file_readlines(lky_func_bundle *bundle)
-{
-    lky_object_function *func = BUW_FUNC(bundle);
-
-    lky_object_custom *self = (lky_object_custom *)func->owner;
-    stlio_file_object_data *data = self->data;
-    
-    FILE *f = data->f;
-
-    arraylist list = arr_create(10);
-
-    char *line = NULL;
-    size_t sz = 0;
-    while(getline(&line, &sz, f) != -1)
-    {
-        // Replace the newline character.
-        line[strlen(line) - 1] = '\0';
-        lky_object *str = stlstr_cinit(line);
-        free(line);
-        arr_append(&list, str);
-        line = NULL;
-        sz = 0;
-    }
-    
-    if(line)
-        free(line);
-
-    lobj_set_member((lky_object *)self, "EOF", lobjb_build_int(1));
-
-    return stlarr_cinit(list);
-}
-
-lky_object *stlio_file_readline(lky_func_bundle *bundle)
-{
-    lky_object_function *func = BUW_FUNC(bundle);
-
-    lky_object_custom *self = (lky_object_custom *)func->owner;
-    stlio_file_object_data *data = self->data;
-
-    FILE *f = data->f;
-
-    char *line = NULL;
-    size_t sz = 0;
-    if(getline(&line, &sz, f) < 0)
-    {
-        lobj_set_member((lky_object *)self, "EOF", lobjb_build_int(1));
-        if(line) free(line);
-        return &lky_nil;
-    }
-
-    line[strlen(line) - 1] = '\0';
-
-    lky_object *ret = stlstr_cinit(line);
-    free(line);
-    return ret;
-}
-
-lky_object *stlio_file_write(lky_func_bundle *bundle)
-{
-    lky_object_function *func = BUW_FUNC(bundle);
-    lky_object_seq *args = BUW_ARGS(bundle);
-
-    lky_object_custom *self = (lky_object_custom *)func->owner;
-    stlio_file_object_data *data = self->data;
-
-    FILE *f = data->f;
-
-    lky_object *b = (lky_object *)args->value;
-    char *line = lobjb_stringify(b, BUW_INTERP(bundle));
-
-    fprintf(f, "%s", line);
-
-    free(line);
-
-    return &lky_nil;
-}
-
-lky_object *stlio_file_writeline(lky_func_bundle *bundle)
-{
-    lky_object_function *func = BUW_FUNC(bundle);
-    lky_object_seq *args = BUW_ARGS(bundle);
-
-    lky_object_custom *self = (lky_object_custom *)func->owner;
-    stlio_file_object_data *data = self->data;
-
-    FILE *f = data->f;
-
-    lky_object *b = (lky_object *)args->value;
-    char *line = lobjb_stringify(b, BUW_INTERP(bundle));
-    
-    fprintf(f, "%s\n", line);
-
-    free(line);
-
-    return &lky_nil;
-}
-
-lky_object *stlio_file_close(lky_func_bundle *bundle)
-{
-    lky_object_function *func = BUW_FUNC(bundle);
-
-    lky_object_custom *self = (lky_object_custom *)func->owner;
-    stlio_file_object_data *data = self->data;
-
-    fclose(data->f);
-    data->f = NULL;
-    return &lky_nil;
-}
-
-lky_object *stlio_make_file_object(lky_func_bundle *bundle)
-{
-    lky_object_seq *args = BUW_ARGS(bundle);
-
-    lky_object_custom *obj = lobjb_build_custom(sizeof(stlio_file_object_data));
-    lky_object *name = (lky_object *)args->value;
-    lky_object *type = (lky_object *)args->next->value;
-
-    if(name->type != LBI_STRING || type->type != LBI_STRING)
-    {
-        // TODO: Error
-    }
-
-    FILE *f = fopen(stlstr_unwrap(name), stlstr_unwrap(type));
-
-    stlio_file_object_data *data = malloc(sizeof(stlio_file_object_data));
-    data->f = f;
-
-    obj->data = data;
-    obj->freefunc = (lobjb_custom_ex_dealloc_function)stlio_file_dealloc;
-    
-    lky_object *gobj = (lky_object *)obj;
-
-    lobj_set_member(gobj, "close", lobjb_build_func_ex(gobj, 0, (lky_function_ptr)stlio_file_close));
-    lobj_set_member(gobj, "getlns", lobjb_build_func_ex(gobj, 0, (lky_function_ptr)stlio_file_readlines));
-    lobj_set_member(gobj, "getln", lobjb_build_func_ex(gobj, 0, (lky_function_ptr)stlio_file_readline));
-    lobj_set_member(gobj, "put", lobjb_build_func_ex(gobj, 1, (lky_function_ptr)stlio_file_write));
-    lobj_set_member(gobj, "putln", lobjb_build_func_ex(gobj, 1, (lky_function_ptr)stlio_file_writeline));
-    lobj_set_member(gobj, "getall", lobjb_build_func_ex(gobj, 0, (lky_function_ptr)stlio_file_readall));
-    lobj_set_member(gobj, "readBytes", lobjb_build_func_ex(gobj, 0, (lky_function_ptr)stlio_file_read_raw));
-    lobj_set_member(gobj, "EOF", lobjb_build_int(f ? 0 : 1));
-
-    return gobj;
-}
-
-lky_object *stlio_get_class()
-{
-    lky_object *obj = lobj_alloc();
-
-    lobj_set_member(obj, "prompt", lobjb_build_func_ex(obj, 1, (lky_function_ptr)stlio_input));
-    lobj_set_member(obj, "put", lobjb_build_func_ex(obj, 1, (lky_function_ptr)stlio_put));
-    lobj_set_member(obj, "putln", lobjb_build_func_ex(obj, 1, (lky_function_ptr)stlio_putln));
-    //lobj_set_member(obj, "printf", lobjb_build_func_ex(obj, 1, (lky_function_ptr)stlio_printf));
-    lobj_set_member(obj, "fopen", lobjb_build_func_ex(obj, 2, (lky_function_ptr)stlio_make_file_object));
-    return obj;
-}
-
-lky_object *stl_io_init()
-{
-    return stlio_get_class();
-}
